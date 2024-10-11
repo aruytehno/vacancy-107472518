8f8e3c9d625d29ed640ccf2da07611c2a534c53f
aruytehno <aruytehno@yandex.ru>
2024-10-11 12:40:31 +0300

    Настроил улучшенное отображение для модели CurrencyRequest, где можно фильтровать и искать записи.

--- currency_project/currency/admin.py
+++ currency_project/currency/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import CurrencyRequest
+from .models import CurrencyRequest  # Импортируем модель
 
-# Register your models here.
-
-# Регистрация модели CurrencyRequest в админке
-admin.site.register(CurrencyRequest)
-
+# Настройка админки для модели CurrencyRequest
+@admin.register(CurrencyRequest)
+class CurrencyRequestAdmin(admin.ModelAdmin):
+    list_display = ('timestamp', 'usd_to_rub')  # Поля, которые будут отображаться в таблице
+    list_filter = ('timestamp',)  # Добавляет возможность фильтровать записи по дате
+    search_fields = ('usd_to_rub',)  # Поле для поиска по курсу доллара
