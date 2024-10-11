4444f9615b8dc21868a7ad5bbb8f16b40c1bf279
aruytehno <aruytehno@yandex.ru>
2024-10-11 12:36:46 +0300

    Отладил работу приложения согласно требованиям ТЗ

--- currency_project/currency/admin.py
+++ currency_project/currency/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import CurrencyRequest
 
 # Register your models here.
+
+# Регистрация модели CurrencyRequest в админке
+admin.site.register(CurrencyRequest)
+
--- currency_project/currency/views.py
+++ currency_project/currency/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+import requests
+import time
+from django.http import JsonResponse
+from .models import CurrencyRequest
+
+API_URL = "https://api.exchangerate-api.com/v4/latest/USD"
+
+def get_current_usd(request):
+    # Задержка 10 секунд между запросами
+    time.sleep(10)
+
+    # Получение данных из API
+    response = requests.get(API_URL)
+    data = response.json()
+    usd_to_rub = data['rates']['RUB']
+
+    # Сохранение в базу данных
+    CurrencyRequest.objects.create(usd_to_rub=usd_to_rub)
+
+    # Получение последних 10 запросов
+    last_10_requests = CurrencyRequest.objects.order_by('-timestamp')[:10]
+
+    # Формирование ответа
+    response_data = {
+        'current_usd_to_rub': usd_to_rub,
+        'last_10_requests': list(last_10_requests.values())
+    }
+
+    return JsonResponse(response_data)
